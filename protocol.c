c497fc91f01d30348f56822cef333e5abca01f39
merberol <simonsson.charlie@gmail.com>
2021-09-26 17:23:33 +0200

    - Raised ENET_PROTOCOL_MAXIMUM_PEER_ID from 0xFFF to 0xFFFF (improved)  BeRo1985 / enet

--- protocol.c
+++ protocol.c
@@ -1012,11 +1012,20 @@ enet_protocol_handle_incoming_commands (ENetHost * host, ENetEvent * event)
     header = (ENetProtocolHeader *) host -> receivedData;
 
     peerID = ENET_NET_TO_HOST_16 (header -> peerID);
+
+   
     sessionID = (peerID & ENET_PROTOCOL_HEADER_SESSION_MASK) >> ENET_PROTOCOL_HEADER_SESSION_SHIFT;
     flags = peerID & ENET_PROTOCOL_HEADER_FLAG_MASK;
     peerID &= ~ (ENET_PROTOCOL_HEADER_FLAG_MASK | ENET_PROTOCOL_HEADER_SESSION_MASK);
-
     headerSize = (flags & ENET_PROTOCOL_HEADER_FLAG_SENT_TIME ? sizeof (ENetProtocolHeader) : (size_t) & ((ENetProtocolHeader *) 0) -> sentTime);
+    /*Change due to upping max connections!!!*/
+    if (peerID == 0xfff)
+    {
+      enet_uint16 * headerPeerID = (enet_uint16 *) & host -> receivedData [headerSize];
+      peerID = *headerPeerID; 
+      headerSize += sizeof (enet_uint16);        
+    }
+    /*change end */
     if (host -> checksum != NULL)
       headerSize += sizeof (enet_uint32);
 
@@ -1567,7 +1576,7 @@ enet_protocol_check_outgoing_commands (ENetHost * host, ENetPeer * peer)
 static int
 enet_protocol_send_outgoing_commands (ENetHost * host, ENetEvent * event, int checkForTimeouts)
 {
-    enet_uint8 headerData [sizeof (ENetProtocolHeader) + sizeof (enet_uint32)];
+    enet_uint8 headerData [sizeof (ENetProtocolHeader) + sizeof (enet_uint16) + sizeof (enet_uint32)]; // changed this line to upp max connections
     ENetProtocolHeader * header = (ENetProtocolHeader *) headerData;
     ENetPeer * currentPeer;
     int sentLength;
@@ -1668,7 +1677,20 @@ enet_protocol_send_outgoing_commands (ENetHost * host, ENetEvent * event, int ch
 
         if (currentPeer -> outgoingPeerID < ENET_PROTOCOL_MAXIMUM_PEER_ID)
           host -> headerFlags |= currentPeer -> outgoingSessionID << ENET_PROTOCOL_HEADER_SESSION_SHIFT;
-        header -> peerID = ENET_HOST_TO_NET_16 (currentPeer -> outgoingPeerID | host -> headerFlags);
+        
+        /* changes due to raising max connections!*/
+        // header -> peerID = ENET_HOST_TO_NET_16 (currentPeer -> outgoingPeerID | host -> headerFlags);
+        if(currentPeer -> outgoingPeerID >= 0xfff)
+        {
+          enet_uint16 * headerPeerID = (enet_uint16 *) & headerData [host -> buffers -> dataLength];
+          * headerPeerID = currentPeer -> outgoingPeerID;          
+          host -> buffers -> dataLength += sizeof(enet_uint16);
+          header -> peerID = ENET_HOST_TO_NET_16 (0xfff | host -> headerFlags);
+        }
+        else
+          header -> peerID = ENET_HOST_TO_NET_16 (currentPeer -> outgoingPeerID | host -> headerFlags);
+        /*changes end*/
+
         if (host -> checksum != NULL)
         {
             enet_uint32 * checksum = (enet_uint32 *) & headerData [host -> buffers -> dataLength];
